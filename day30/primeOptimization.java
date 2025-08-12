f65aeee5c2f5cb8a542e55a5d995d402234636b4
Brindha2206 <brindha220605@gmail.com>
2025-08-12 15:55:45 +0530

    day30-2

--- day30/primeOptimization.java
+++ day30/primeOptimization.java
@@ -2,6 +2,9 @@ package day30;
 
 import java.util.Scanner;
 
+import day29.sortingFactorsinDescOrder;
+
+import java.util.*;
 public class primeOptimization {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -36,29 +39,48 @@ public class primeOptimization {
     // }
 
     //4 
-     if(n==2||n==3){
-        System.out.println("Prime");
-    }
-     else if(n%2==0){
-            System.out.println("Not a Prime");
-        }
-        else{
-            for(int i = 4;i<n;i+=3){
-                count++;
-            }
-            System.out.println((count==0)?("Prime"):("Not a Prime"));
+    //  if(n==2||n==3){
+    //     System.out.println("Prime");
+    // }
+    //  else if(n%2==0){
+    //         System.out.println("Not a Prime");
+    //     }
+    //     else{
+    //         for(int i = 4;i<n;i+=3){
+    //             count++;
+    //         }
+    //         System.out.println((count==0)?("Prime"):("Not a Prime"));
            
-}
+// }
 //5 O(n/2)
-int c =1;
-for(int i = 2;i<=n/2;i++){
-    if(n%i==0){
-        c=0;
-        break;
-    }
-}
- System.out.println((c==1)?("Prime"):("Not a Prime"));
+// int c =1;
+// for(int i = 2;i<=n/2;i++){
+//     if(n%i==0){
+//         c=0;
+//         break;
+//     }
+// }
+//  System.out.println((c==1)?("Prime"):("Not a Prime"));
  //6 O(root-n)
-  
+//   int cnt =1,sq=(int)Math.sqrt(n);;
+// for(int i = 2;i<=sq;i++){
+//     if(n%i==0){
+//         cnt=0;
+//         break;
+//     }
+// }
+//  System.out.println((cnt==1)?("Prime"):("Not a Prime"));
+
+ //7. O(root-n/2)
+   int sqs=(int)Math.sqrt(n);
+   int i ;
+   if(n%2==0&&n>2){
+    System.out.println("Not a prime");
+   }
+   else{
+    for( i =3;i<=sqs && n%i!=0;i+=2);
+    System.out.println((i>sqs)?("prime"):("Not prime"));
+} 
+
 }
 }
